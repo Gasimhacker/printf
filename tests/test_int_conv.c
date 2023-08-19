8268bfac69b1fbf29c63f70850457fc8bd1d1c54
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-19 22:35:45 +0000

    Create the testing directory and add the first test

---
+++ tests/test_int_conv.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Test passing an integer to _printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len1, len2;
+
+	len1 = _printf("My age is %d", -23);
+	len2 = printf("My age is %d", -23);
+	printf("Length1:[%d], Length2:[%d]\n", n, num);
+
+	return (0);
+}
