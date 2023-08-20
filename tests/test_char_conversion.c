5ac2fe45c68b389d96f2bf1c1bf7bc35472a10f8
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-20 09:50:50 +0000

    Test the "c" and "s" conversion specifiers

---
+++ tests/test_char_conversion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Test passing a character to _printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len1, len2;
+
+	len1 = _printf("My secret letter is %c", 'A');
+	len2 = printf("My secret letter is %c", 'A');
+	printf("Length1:[%d], Length2:[%d]\n", len1, len2);
+
+	return (0);
+}
---
+++ tests/test_str_conversion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Test passing a character to _printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len1, len2;
+
+	len1 = _printf("My secret string is %s", "ROT13");
+	len2 = printf("My secret string is %s", "ROT13");
+	printf("Length1:[%d], Length2:[%d]\n", len1, len2);
+
+	return (0);
+}
