f0e40c6dba2a8d3ff5cbb701dc6d0cd9b60c5ab9
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-21 22:11:03 +0000

    Add the deleted files main.h and base_convertors.c

---
+++ base_converters.c
@@ -0,0 +1,96 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * to_binary - Convert an unsigned integer to binary
+ * @n: The number to be converted
+ *
+ * Return: A pointer to the binary
+ *	   representation (as a string) of the argument passed
+ */
+char *to_binary(unsigned int n)
+{
+	int i = 32;
+	char ans[33];
+	char *ptr = ans;
+
+	ans[32] = '\0';
+
+	if (!n)
+		ans[--i] = '0';
+
+	while (i && n)
+	{
+		ans[--i] = (n % 2) + '0';
+		n /= 2;
+	}
+
+	return (ptr + i);
+
+}
+
+/**
+ * convert_to_base - an unsigned integer to binary
+ * @number: The number to be converted
+ * @base: The base of the number to be converted to
+ * @capital: Determine the state of the chars 0 = print small , 1 = prints capital
+ *
+ * Return: A to the binary
+ *	   representation (as a string) of the argument passed
+ */
+char *convert_to_base(unsigned int number, int base, int capital)
+{
+	char *result, *buffer;
+	int index, i, j;
+	unsigned int remainder;
+
+	buffer = malloc(32 * sizeof(char));
+	index = 0;
+
+	if (buffer == NULL)
+		return (NULL);
+
+	if (number == 0)
+	{
+		buffer[index++] = '0';
+	}
+	else
+	{
+		while (number != 0)
+		{
+			remainder = number % base;
+			if (remainder < 10)
+			{
+				buffer[index++] = (remainder + '0');
+			}
+			else
+			{
+				if (capital)
+				{
+					buffer[index++] = (remainder + 'A' - 10);
+				}
+				else
+				{
+					buffer[index++] = (remainder + 'a' - 10);
+				}
+			}
+			number /= base;
+		}
+	}
+
+	result = malloc((index + 1) * sizeof(char));
+
+	if (result == NULL)
+		return (NULL);
+
+	for (i = index - 1, j = 0; i >= 0; i--, j++)
+	{
+		result[j] = buffer[i];
+	}
+	result[j] = '\0';
+
+	free(buffer);
+
+	return (result);
+}
