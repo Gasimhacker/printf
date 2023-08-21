c7f9ea17bce07e23a199117f62edfd4e07e048d4
Obada A.Albadee <obadabadee3@hotmail.com>
2023-08-21 11:33:41 +0300

    The hex conversion function will print

--- main.h
+++ main.h
@@ -77,10 +77,10 @@ char *to_binary(unsigned int n);
 int print_octal(va_list args);
 
 /**
- * print_capital_hex - Prints the octal representation of an unsigned integer
- * @args: A list containing the integer to print its octal representation
+ * print_capital_hex - Prints the capital hexadecimal representation of an unsigned integer
+ * @args: A list containing the integer to print its hexadecimal representation
  *
- * Return: The length of the printed octal number
+ * Return: The length of the printed hexadecimal number
  */
 int print_capital_hex(va_list args);
 
