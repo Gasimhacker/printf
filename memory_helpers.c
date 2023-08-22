a94d0b32a4480459d97fe2b787fed36bea895499
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-22 10:25:35 +0000

    Add functions to initialize, free, and copy to the buffer

--- memory_helpers.c
+++ memory_helpers.c
@@ -1,40 +1,74 @@
-/**
- * _strlen - Returns the length of the string
- * @s: The string to calculate its length
- *
- * Return: The length of the string
- */
-int _strlen(char *s)
-{
-	int len = 0;
+#include "main.h"
 
-	while (*(s + len))
+
+/**
+ * _memcpy - Copy n bytes from source to the buffer contained in the buffer_t struct
+ * @output: A pointer to the buffer_t struct
+ * @src: A pointers to the bytes to be copied
+ * @n: The number of the copied bytes
+ *
+ * Return: The number of bytes copied
+ */
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
 	{
-		len++;
+		*(output->buffer) = *(src + index);
+		(output->len)++;
+
+		if (output->len == 1024)
+		{
+			write(1, output->start, output->len);
+			output->buffer = output->start;
+			output->len = 0;
+		}
+		else
+		{
+			(output->buffer)++;
+		}
 	}
 
-	return (len);
+	return (n);
 }
 
 
 /**
- * _strcpy - Copy a string in one variable into another variable
- * @src: The string to be copied
- * @dest: The destination of the copied string
+ * free_buffer - Free the allocated memory for the buffer_t struct
+ * @output: The buffer to be freed
  *
- * Return: The pointer to the destination.
+ * Return: void
  */
-char *_strcpy(char *dest, char *src)
+void free_buffer(buffer_t *output)
 {
-	char *ptr = dest;
+	free(output->start);
+	free(output);
+}
 
-	while ((*src) != '\0')
+
+/**
+ * init_buffer - Initialize a variable of of type buffer_t (a struct)
+ *
+ * Return: A pointer to the initialized struct
+ */
+buffer_t *init_buffer(void)
+{
+	buffer_t *output;
+
+	output = malloc(sizeof(buffer_t));
+	if (output == NULL)
+		return (NULL);
+
+	output->buffer = malloc(sizeof(char) * 1024);
+	if (output->buffer == NULL)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		free(output);
+		return (NULL);
 	}
-	*dest = '\0';
 
-	return (ptr);
+	output->start = output->buffer;
+	output->len = 0;
+
+	return (output);
 }
