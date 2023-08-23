6c320a077cc14d972daf11ee00af4fe2bdd6eaba
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-23 01:59:48 +0000

    Fix the problem of zero padding with precision

--- base_converter.c
+++ base_converter.c
@@ -1,5 +1,58 @@
 #include "main.h"
 #include <stdio.h>
+
+
+
+unsigned int init_handlers(buffer_t *output, int ret, unsigned char flags,
+		int wid, int prec)
+{
+	char pad, *lead = "0x";
+
+	if (prec > 0)
+	{
+		if (NEG_FLAG == 0) /* Handle width */
+		{
+			pad = ' ';
+			for (; wid > 0; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
+
+
+		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
+			ret += _memcpy(output, lead, 2);
+
+		pad = '0';
+
+		for (; prec > 1; prec--) /* Handle precision */
+			ret += _memcpy(output, &pad, 1);
+	}
+	else
+	{
+
+
+		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
+			ret += _memcpy(output, lead, 2);
+
+		if (NEG_FLAG == 0) /* Handle width */
+		{
+			pad = (prec <= -1)? '0' : ' ';
+
+			for (; wid > 0; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
+
+		pad = '0';
+
+		for (; prec > 1; prec--) /* Handle precision */
+			ret += _memcpy(output, &pad, 1);
+
+	}
+
+
+
+	return (ret);
+}
+
 /**
  * convert_sig_base - Convert a signed long to an inputted base
  *		      and store the result to a buffer inside buffer_t
@@ -16,7 +69,7 @@ unsigned int convert_sig_base(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec)
 {
 	int size;
-	char digit, pad = '0';
+	char digit;
 	unsigned int ret = 1;
 
 	for (size = 0; *(base + size);)
@@ -30,18 +83,10 @@ unsigned int convert_sig_base(buffer_t *output, long int num, char *base,
 	{
 
 
-		wid -= (prec <= -1) ? 0 : prec;
+		wid -= (prec <= -1) ? 1 : prec;
+		ret = init_handlers(output, ret, flags, wid, prec);
+		
 
-		if (NEG_FLAG == 0) /* Handle width */
-		{
-			pad = (ZERO_FLAG == 1) ? '0' : ' ';
-			for (; wid > 0; wid--)
-				ret += _memcpy(output, &pad, 1);
-		}
-
-		pad = '0';
-		for (; prec > 1; prec--, wid--) /* Handle precision */
-			ret += _memcpy(output, &pad, 1);
 	}
 
 	digit = base[(num < 0 ? -1 : 1) * (num % size)];
@@ -66,7 +111,7 @@ unsigned int convert_usig_base(buffer_t *output, unsigned long int num,
 		char *base, unsigned char flags, int wid, int prec)
 {
 	unsigned int size, ret = 1;
-	char digit, pad = '0', *lead = "0x";
+	char digit;
 
 	for (size = 0; *(base + size);)
 		size++;
@@ -84,21 +129,7 @@ unsigned int convert_usig_base(buffer_t *output, unsigned long int num,
 
 
 		wid -= (prec <= -1) ? 1 : prec;
-
-		if (NEG_FLAG == 0) /* Handle width */
-		{
-			pad = (ZERO_FLAG == 1) ? '0' : ' ';
-			for (; wid > 0; wid--)
-				ret += _memcpy(output, &pad, 1);
-		}
-
-		pad = '0';
-
-		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
-			ret += _memcpy(output, lead, 2);
-
-		for (; prec > 1; prec--) /* Handle precision */
-			ret += _memcpy(output, &pad, 1);
+		ret = init_handlers(output, ret, flags, wid, prec);
 
 	}
 
