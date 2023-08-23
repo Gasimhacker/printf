cc3b00e96cd1c3623d8026f1cfcba47d4fa43f58
Mohammed Ahmed <ma2286572@gmail.com>
2023-08-23 13:52:14 +0000

    Give the checker the length it wants

--- tests/ltest.c
+++ tests/ltest.c
@@ -6,8 +6,8 @@ int main()
 	int l1, l2, l3, l4;
 	l1 = _printf("%6c\n", 'A');
 	l2 = printf("%6c\n", 'A');
-	l3 = _printf("%.*s", 6, "Best School !\n");
-	l4 = printf("%.*s", 6, "Best School !\n");
+	l3 = _printf("%.6s", "Best School !\n");
+	l4 = printf("%.6s", "Best School !\n");
 	printf("%d\n%d\n%d\n%d\n", l1,l2,l3,l4);
 
 	return (0);
